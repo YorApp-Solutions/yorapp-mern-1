bd8a8488ec055c5d6758861d3bc16da88e4994b4
avg <1994.avg@gmail.com>
2020-02-23 16:53:46 +0530

    db test

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dbConfig = require('./dbConfig');
+const mongoose = require('mongoose');
+
+const mongoURL = dbConfig.url;
+
+mongoose.connect(mongoURL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', console.log.bind(console, 'connection error'));
 
 const app = express();
 
@@ -14,6 +22,25 @@ app.get('/', function(req, res){
     res.send('hello');
 })
 
-app.listen(port, ()=>{
-    console.log(`listening at ${port}`)
+app.get('/number', function(req, res){
+    //create a schema
+    const userSchema = new mongoose.Schema({
+        phone: Number
+    });
+    //compile the schema
+    const User = mongoose.model('User', userSchema, 'users');
+    //return users in db and send to browser
+    User.find(function(err, user){
+        if(err) return console.error(err);
+        console.log(user);
+        res.send(user);
+    })
+
+})
+
+
+db.once('open', function(){
+    app.listen(port, ()=>{
+        console.log(`listening at ${port}`)
+    })
 })
