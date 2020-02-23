3ccb7795211acad9e200566b227b7226d6b0f32c
avg <1994.avg@gmail.com>
2020-02-23 16:16:07 +0530

    test ignore

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 const port = 8000;
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static('public'));
+
 
 app.get('/', function(req, res){
     res.send('hello');
